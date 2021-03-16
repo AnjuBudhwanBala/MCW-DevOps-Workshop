eafb3f6a37ae846181e28cb6e5a6c2281d832521
Ove Bastiansen <ove.bastiansen@banqsoft.com>
2021-03-16 10:16:47 +0100

    updated description

--- src/TailspinToysWeb/Data/ProductService.cs
+++ src/TailspinToysWeb/Data/ProductService.cs
@@ -10,7 +10,7 @@ namespace TailspinToysWeb.Data
         {
             Product[] products = new Product[4]
             {
-                new Product(1, "Outtop Foam Throwing Glider", "Foam throwing glider airplane", 10),
+                new Product(1, "Outtop Foam Throwing Glider!", "Foam throwing glider airplane", 20),
                 new Product(2, "Fascinations Metal Earth 3D Laser Cut Model", "Wright Brothers airplane made of 3D laser cut metal", 20),
                 new Product(3, "Vintage Model USMC Corsair Model Airplane", "Complete kit with realistic marking scheme", 30),
                 new Product(4, "Delta Diecast Model Airplane", "Delta Single Plane Replica", 40)
